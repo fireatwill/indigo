fae63e78150cfd02bd5f40e2d283cb6a055a775c
Will Wright <will@willwright.net>
2025-09-19 18:14:38 +0100

    More of the basic driver outline

--- indigo_drivers/dome_pulsar/indigo_dome_pulsar.c
+++ indigo_drivers/dome_pulsar/indigo_dome_pulsar.c
@@ -19,7 +19,82 @@
 
 #include "indigo_dome_pulsar.h"
 
+#define PRIVATE_DATA			((pulsar_private_data *)device->private_data)
+
+typedef struct {
+	int handle;
+} pulsar_private_data;
+
+static indigo_result dome_enumerate_properties(indigo_device *device, indigo_client *client, indigo_property *property) {
+	return indigo_dome_enumerate_properties(device, NULL, NULL);
+}
+
+static indigo_result dome_attach(indigo_device *device) {
+	assert(device != NULL);
+	assert(PRIVATE_DATA != NULL);
+
+	return INDIGO_FAILED;
+}
+
+static indigo_result dome_change_property(indigo_device *device, indigo_client *client, indigo_property *property) {
+	assert(device != NULL);
+	return indigo_dome_change_property(device, client, property);
+}
+
+static indigo_result dome_detach(indigo_device *device) {
+	assert(device != NULL);
+	return indigo_dome_detach(device);
+}
+
+// --------------------------------------------------------------------------------
+
+static pulsar_private_data *private_data = NULL;
+
+static indigo_device *dome = NULL;
+
 indigo_result indigo_dome_pulsar(indigo_driver_action action, indigo_driver_info *info) {
+	static indigo_device dome_template = INDIGO_DEVICE_INITIALIZER(
+		DOME_PULSAR_NAME,
+		dome_attach,
+		dome_enumerate_properties,
+		dome_change_property,
+		NULL,
+		dome_detach
+	);
+
+	static indigo_driver_action last_action = INDIGO_DRIVER_SHUTDOWN;
+
+	SET_DRIVER_INFO(info, DOME_PULSAR_NAME, __FUNCTION__, DRIVER_VERSION, false, last_action);
+
+	if (action == last_action)
+		return INDIGO_OK;
+
+	switch(action) {
+		case INDIGO_DRIVER_INIT:
+			last_action = action;
+			private_data = indigo_safe_malloc(sizeof(pulsar_private_data));
+			dome = indigo_safe_malloc_copy(sizeof(indigo_device), &dome_template);
+			dome->private_data = private_data;
+			indigo_attach_device(dome);
+			break;
+
+		case INDIGO_DRIVER_SHUTDOWN:
+			VERIFY_NOT_CONNECTED(dome);
+			last_action = action;
+			if (dome != NULL) {
+				indigo_detach_device(dome);
+				free(dome);
+				dome = NULL;
+			}
+			if (private_data != NULL) {
+				free(private_data);
+				private_data = NULL;
+			}
+			break;
+
+		case INDIGO_DRIVER_INFO:
+			break;
+	}
 
 	return INDIGO_OK;
 }
